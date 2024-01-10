a1e2cd23cc3cba0dd4d74a1574b1754dffd16bd7
shingostar <shingostar@shore.ocn.ne.jp>
2024-01-10 22:38:19 +0900

    コミットします。

---
+++ src/ip/co/kiramex/dbSample/entity/Country.java
@@ -0,0 +1,34 @@
+package ip.co.kiramex.dbSample.entity;
+
+public class Country {
+    //  フィールド
+    private String name;
+    private int population;
+
+    //  引数なしコンストラクタ
+    public Country() {
+    }
+
+    //  引数ありコンストラクタ
+    public Country(String name,int population) {
+        this.name = name;
+        this.population = population;
+    }
+
+    //getter/setter
+    public String getName(String name) {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void  setPopulation(int population) {
+        this.population = population;
+    }
+}
